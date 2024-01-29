7be6a7f402ec28a9e01886ba431550ba9e492b33
ThanhhTruongg1901 <thanhtruong1901@gmail.com>
2024-01-30 01:03:49 +0700

    design all screens

--- app/src/main/java/com/example/newsapp/fragment/favoritefragment.kt
+++ app/src/main/java/com/example/newsapp/fragment/favoritefragment.kt
@@ -35,7 +35,7 @@ class favoritefragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_favoritefragment, container, false)
+        return inflater.inflate(R.layout.fragment_favorite, container, false)
     }
 
     companion object {
